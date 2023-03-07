b4f038dbd747b817f4d6e71fb9d9d51fd4fd6baf
hililyy <joun406@gmail.com>
2023-03-08 03:19:29 +0900

    [FEAT] Firebase 설정 및 Splash 만들기

--- LilyTalk/LilyTalk/LilyTalk/ViewController.swift
+++ LilyTalk/LilyTalk/LilyTalk/ViewController.swift
@@ -6,14 +6,78 @@
 //
 
 import UIKit
+import SnapKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    var box = UIImageView()
+    var remoteConfig: RemoteConfig!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        remoteConfig = RemoteConfig.remoteConfig()
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 0
+        remoteConfig.configSettings = settings
+        remoteConfig.setDefaults(fromPlist: "RemoteConfigDefaults")
+        
+        remoteConfig.fetch(withExpirationDuration: TimeInterval(0)) {
+            (status, error) -> Void in
+          if status == .success {
+            print("Config fetched!")
+            self.remoteConfig.activate { changed, error in
+              // ...
+            }
+          } else {
+            print("Config not fetched")
+            print("Error: \(error?.localizedDescription ?? "No error available.")")
+          }
+          self.displayWelcome()
+        }
+        
+        self.view.addSubview(box)
+        box.snp.makeConstraints { make in
+            make.center.equalTo(self.view)
+        }
+        box.image = #imageLiteral(resourceName: "ic_loading")
+//        self.view.backgroundColor = UIColor(hex: "#0000000")
     }
-
-
+    
+    func displayWelcome() {
+        let color = remoteConfig["splash_background"].stringValue
+        let caps = remoteConfig["splash_message_caps"].boolValue
+        let message = remoteConfig["splash_message"].stringValue
+        
+        if caps {
+            let alert = UIAlertController(title: "공지사항", message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style: .default, handler: { action in
+                exit(0)
+            }))
+            self.present(alert, animated: true)
+        }
+        self.view.backgroundColor = UIColor(hex: color!)
+    }
 }
 
+
+extension UIColor {
+    convenience init(hex: String) {
+        let scanner = Scanner(string: hex)
+        scanner.scanLocation = 1
+        
+        var rgbValue: UInt64 = 0
+        
+        scanner.scanHexInt64(&rgbValue)
+        
+        let r = (rgbValue & 0xff0000) >> 16
+        let g = (rgbValue & 0xff00) >> 8
+        let b = rgbValue & 0xff
+        
+        self.init(
+            red: CGFloat(r) / 0xff,
+            green: CGFloat(g) / 0xff,
+            blue: CGFloat(b) / 0xff, alpha: 1
+        )
+    }
+}
