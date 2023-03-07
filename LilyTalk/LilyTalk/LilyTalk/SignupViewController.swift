210bc4a09cdd2167ccddfac3e5422f0380ab7fbd
hililyy <joun406@gmail.com>
2023-03-08 06:04:10 +0900

    [FEAT] 회원가입 추가 (Firebase storage에 이미지 파일 보내기)

--- LilyTalk/LilyTalk/LilyTalk/SignupViewController.swift
+++ LilyTalk/LilyTalk/LilyTalk/SignupViewController.swift
@@ -8,9 +8,11 @@
 import UIKit
 import Firebase
 import FirebaseDatabase
+import FirebaseStorage
 
-class SignupViewController: UIViewController {
+class SignupViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var password: UITextField!
@@ -30,6 +32,10 @@ class SignupViewController: UIViewController {
         }
         color = remoteConfig["splash_background"].stringValue
         statusBar.backgroundColor = UIColor(hex: color)
+        
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(imagePicker)))
+        
         signupBtn.backgroundColor = UIColor(hex: color)
         cancelBtn.backgroundColor = UIColor(hex: color)
         
@@ -37,12 +43,37 @@ class SignupViewController: UIViewController {
         cancelBtn.addTarget(self, action: #selector(cancelEvent), for: .touchUpInside)
     }
     
+    @objc func imagePicker() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+        
+        self.present(imagePicker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = (info[UIImagePickerController.InfoKey.originalImage] as! UIImage)
+        dismiss(animated: true)
+    }
+    
     @objc func signupEvent() {
-        Auth.auth().createUser(withEmail: email.text ?? "", password: password.text ?? "") { user, error in
+        
+        Auth.auth().createUser(withEmail: email.text!, password: password.text!) { user, err in
             let uid = user?.user.uid
-            Database.database().reference().child("users").child(uid!).setValue(["name":self.name.text])
+            let image = self.imageView.image!.jpegData(compressionQuality: 0.1)
+            
+            let imageRef = Storage.storage().reference().child("userImages").child(uid!)
+            
+            imageRef.putData(image!, metadata: nil) { (metadata, error) in
+                imageRef.downloadURL { (url, error) in
+                    guard let downloadURL = url else { return }
+                    Database.database().reference().child("user").child(uid!).setValue(["userName" : self.name.text, "profileImageUrl" : downloadURL.absoluteString])
+                }
+            }
         }
     }
+    
     @objc func cancelEvent() {
         self.dismiss(animated: true)
     }
